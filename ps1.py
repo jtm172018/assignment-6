45774d91c44440341e2dc243ba3147b7437a6411
jtm172018 <chanchalkumar15210@gmail.com>
2017-09-12 13:12:38 +0530

    first time commited

--- ps1.py
+++ ps1.py
@@ -1,3 +1,7 @@
-###### this is the first .py file ###########
+###### Assignment 1 piraty check and framing .py file ###########
 
 ####### write your code here ##########
+x= input(Enter binary data which has to be transmitted.)
+
+
+
